b0d6ca7f9a39f15d74707a660fc409505a59b3d2
Forhad Hussain <furhadhussain@gmail.com>
2022-07-21 14:48:01 +0100

    completed assignment

--- src/soft-play.js
+++ src/soft-play.js
@@ -1,6 +1,6 @@
 // do not change these lines
 
-function reset () {
+function reset() {
   adults = 0
   children = 0
 }
@@ -11,10 +11,45 @@ let children = 0
 // TODO: Write your functions in the below section. Your functions should update
 // the adults and children variables defined above.
 
+function enter(numAdults, numChildren) {
+  if (numAdults >= numChildren) {
+    adults = adults + numAdults
+    children = children + numChildren
+
+    return true
+  } else if (numAdults < numChildren) {
+    return false
+  }
+}
+
+function leave(numAdults, numChildren) {
+  const childrenRemaining = children - numChildren
+  const adultsRemaining = adults - numAdults
+
+  if (
+    numAdults >= numChildren &&
+    numAdults <= adults &&
+    numChildren <= children &&
+    adultsRemaining >= childrenRemaining
+  ) {
+    adults = adultsRemaining
+    children = childrenRemaining
+
+    return true
+  } else {
+    return false
+  }
+}
+function occupancy() {
+  return {
+    adults: adults,
+    children: children
+  }
+}
 // TODO: Change the undefined values below to the name of your functions
 module.exports = {
-  enter: undefined,
-  leave: undefined,
-  occupancy: undefined,
-  reset: reset
+  enter,
+  leave,
+  occupancy,
+  reset
 }
